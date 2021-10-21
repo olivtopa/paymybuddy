b6424979cd6985efeed6545c19b01b0d15bcbf76
Olivier <amoliv_825@hotmail.com>
2021-10-21 15:21:22 +0200

    creation des classes du model

---
+++ src/main/java/com/olivtopa/paymybuddy/model/MoneyTransaction.java
@@ -0,0 +1,57 @@
+package com.olivtopa.paymybuddy.model;
+
+import java.util.Date;
+
+public class MoneyTransaction {
+	
+	private String emailOrigin;
+	private String emailRecipient;
+	private String description;
+	private Double amount;
+	private Date creationDate;
+	
+	public void recordTransaction() {
+		//TODO
+	}
+
+	public String getEmailOrigin() {
+		return emailOrigin;
+	}
+
+	public void setEmailOrigin(String emailOrigin) {
+		this.emailOrigin = emailOrigin;
+	}
+
+	public String getEmailRecipient() {
+		return emailRecipient;
+	}
+
+	public void setEmailRecipient(String emailRecipient) {
+		this.emailRecipient = emailRecipient;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+}
---
+++ src/main/java/com/olivtopa/paymybuddy/model/User.java
@@ -0,0 +1,56 @@
+package com.olivtopa.paymybuddy.model;
+
+import java.util.List;
+
+public class User {
+	
+	private String email;
+	private String password;
+	private Double solde;
+	private List<Contact> contact;
+	
+	public Contact addContact() {
+		return null;
+	}
+	
+	public void tranfert() {
+		//TODO
+	}
+	
+	public void fund() {
+		//TODO
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(Double solde) {
+		this.solde = solde;
+	}
+
+	public List<Contact> getContact() {
+		return contact;
+	}
+
+	public void setContact(List<Contact> contact) {
+		this.contact = contact;
+	}
+
+}
