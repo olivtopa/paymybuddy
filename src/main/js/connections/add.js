2397c3f75d2da15f9a048f1adab3858677b0b708
Olivier <amoliv_825@hotmail.com>
2021-11-11 12:26:29 +0100

    ajout du champ emailOrigin dans AddConnectionPopup

--- src/main/js/connections/add.js
+++ src/main/js/connections/add.js
@@ -2,46 +2,52 @@ import React from "react";
 
 // TODO
 export default class AddConnectionPopup extends React.Component {
-	
-	constructor (props) {
+
+	constructor(props) {
 		super(props);
-		this.state = {emailContact:null, connection:null};
+		this.state = { emailOrigin: null, emailContact: null, connection: null };
+	}
+
+	enterEmailOrigin = (emailOrigin) => {
+		console.log('your new email: ' + emailOrigin);
+		this.setState(prevState => ({ ...prevState, emailOrigine: String(emailOrigin) }));
 	}
 	
 	enterEmailContact = (emailContact) => {
-		console.log('your new email contact: '+ emailContact);
-		this.setState(prevState => ({...prevState, contactEmail: String(contactEmail)}));
+		console.log('your new email contact: ' + emailContact);
+		this.setState(prevState => ({ ...prevState, emailContact: String(emailContact) }));
 	}
-	
+
 	enterConnection = (connection) => {
-		console.log('your new connection: '+ connection);
-		this.setState(prevState => ({...prevState, connection: String(connection)}));
+		console.log('your new connection: ' + connection);
+		this.setState(prevState => ({ ...prevState, connection: String(connection) }));
 	}
-	
-	
+
+
 
 	addConnection = (connectionRequest) => {
 		console.log(['Adding connection', connectionRequest]);
-		const entity = {emailContact:connectionRequest.emailContact, connection:connectionRequest.connection};
+		const entity = { emailOrigin:connectionRequest.emailOrigin, emailContact: connectionRequest.emailContact, connection: connectionRequest.connection };
 		axios.post('/addContact', entity).then(() => {
 			console.log('connection added');
 		}
-		
+
 		)
 	}
 
 
-render() {
+	render() {
 
-	return (
-		<div>
-			<h1>Add a new connection</h1>
-			<form>
-				<input type="text" onInput={(e) => this.enterEmailContact(e.target.value)}/>
-				<input type="text" onInput={(e) => this.enterConnection(e.target.value)}/>
-				<button>Submit</button>
-			</form>
-		</div>
-	)
-}
+		return (
+			<div>
+				<h1>Add a new connection</h1>
+				<form>
+					<input type="text" onInput={(e) => this.enterEmailOrigin(e.target.value)} />
+					<input type="text" onInput={(e) => this.enterEmailContact(e.target.value)} />
+					<input type="text" onInput={(e) => this.enterConnection(e.target.value)} />
+					<button>Submit</button>
+				</form>
+			</div>
+		)
+	}
 }
