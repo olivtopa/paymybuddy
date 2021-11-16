8f63ec71cf407893d592cbdacd270005eea713a5
Olivier <amoliv_825@hotmail.com>
2021-11-16 15:31:59 +0100

    modification du test unitaire

--- src/test/java/com/olivtopa/paymybuddy/service/test/TransactionTest.java
+++ src/test/java/com/olivtopa/paymybuddy/service/test/TransactionTest.java
@@ -1,20 +1,34 @@
 package com.olivtopa.paymybuddy.service.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.olivtopa.paymybuddy.model.Contact;
 import com.olivtopa.paymybuddy.model.Transaction;
 import com.olivtopa.paymybuddy.model.User;
+import com.olivtopa.paymybuddy.service.ContactService;
 import com.olivtopa.paymybuddy.service.TransactionService;
+import com.olivtopa.paymybuddy.service.UserService;
 
 @ExtendWith(MockitoExtension.class)
 public class TransactionTest {
 
 	@InjectMocks
 	private TransactionService transactionService;
+	
+	@Mock
+	UserService userService;
+	@Mock
+	ContactService contactService;
 
 	@Test
 	public void transactionTest() {
@@ -23,19 +37,29 @@ public class TransactionTest {
 		Transaction transaction = new Transaction();
 		transaction.setEmailOrigin("email1@gmail.com");
 		transaction.setEmailContact("email2@gmail.com");
-		transaction.setAmount(100);
+		transaction.setAmount(100.00);
 		
 		User userOrigin = new User();
-		userOrigin.setSolde(10.00);
+		userOrigin.setEmail("email1@gmail.com");
+		userOrigin.setSolde(200.00);
 		
 		User userContact = new User();
+		userContact.setEmail("email2@gmail.com");
 		userContact.setSolde(0);
+		List<User> userContacts = new ArrayList<>();
+		userContacts.add(userContact);
+		
+		
+		
+		
+		Mockito.when(userService.getUserByEmail(ArgumentMatchers.anyString())).thenReturn(userOrigin);
+		//Mockito.when(contactService.getContactsByUser(ArgumentMatchers.anyString())).thenReturn(userContact);
 
 		// When
 		transactionService.transaction(transaction);
 
 		// Then
-		Assertions.assertThat(userContact.getSolde()).isEqualTo(0);
+		Assertions.assertThat(userContact.getSolde()).isEqualTo(100);
 
 	}
 
