25e23639a3444103366d4d7d0a650ba5adbcaac7
Olivier <amoliv_825@hotmail.com>
2021-12-03 10:26:20 +0100

    correction de data.sql

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -6,6 +6,10 @@ VALUES('email1@gmail.com','$2a$10$Xb9WVbjDJfz094nb.5Il7.HQKiaMiWtMWL0YON8l2JlYnl
 ('email22@gmail.com','$2a$10$98mCdTaiNJBKQUE2DgZslu3kizWPViUtxHdsSpxOU/J70AxB6GRd2','800'),
 ('email33@gmail.com','$2a$10$ftI13fnKlpbmokhJQvGUA.cqn5agxvbtMhgGbIR8xD9KhxRhmMTJO','60');
 
+INSERT INTO contact(email_origin,email_contact,name)
+VALUES('email1@gmail.com','email11@gmail.com','contact1'),
+('email2@gmail.com','email22@gmail.com','contact2'),
+('email3@gmail.com','email33@gmail.com','contact3');
 
 
 INSERT INTO money_transaction(email_origin,email_recipient,description,creation_date,amount)
@@ -14,7 +18,4 @@ VALUES('email1@gmail.com','email11@gmail.com','transaction number 1','2020-10-27
 ('email3@gmail.com','email33@gmail.com','transaction number 3','2020-10-29 13:18:00','200');
 
 
-INSERT INTO contact(email_origin,email_contact,name)
-VALUES('email1@gmail.com','email11@gmail.com','contact1'),
-('email2@gmail.com','email22@gmail.com','contact2'),
-('email3@gmail.com','email33@gmail.com','contact3');
+
