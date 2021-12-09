77f30e1f2ae2be7a8393c53fddfad307072e419b
Olivier <amoliv_825@hotmail.com>
2021-12-09 16:26:29 +0100

    correction de UserController

--- src/main/java/com/olivtopa/paymybuddy/controller/UserController.java
+++ src/main/java/com/olivtopa/paymybuddy/controller/UserController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,22 +20,22 @@ public class UserController {
 
 	@Autowired
 	UserService userService;
-	
-	@Autowired LoginService loginService;
-	
+
+	@Autowired
+	LoginService loginService;
+
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-	
-	@GetMapping(value= "/api/user/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public User getUserByEmail(@PathVariable("email") String email){
+
+	@GetMapping(value = "/api/user/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public User getUserByEmail(@PathVariable("email") String email) {
 		logger.info("user {} :", email);
 		return userService.getUserByEmail(email);
 	}
-	
-	@PostMapping(value= "/api/login", consumes =MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@PostMapping(value = "/api/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public void login(@RequestBody User user) throws LoginException {
-		logger.info("connexion {} : ",user.getEmail());
-		//user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+		logger.info("connexion {} : ", user.getEmail());
 		loginService.loginControle(user);
 	}
-	
+
 }
