e4322ae008864306796160b47b8ade46297fc319
Olivier <amoliv_825@hotmail.com>
2021-12-24 17:09:25 +0100

    Creation page Login Spring

--- src/main/java/com/olivtopa/paymybuddy/configuration/SpringSecurityConfig.java
+++ src/main/java/com/olivtopa/paymybuddy/configuration/SpringSecurityConfig.java
@@ -1,22 +1,28 @@
 package com.olivtopa.paymybuddy.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http
-		.authorizeRequests()
-		.anyRequest().authenticated()
-		.and()
-		.formLogin();
-		
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource);
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+	}
 }
